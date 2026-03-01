5c6645fea2cd21bc09232d07691c5ad5801c890e
gabrielberlucci <gabrielberluccidev@proton.me>
2026-02-28 21:02:55 -0300

    feat: add sidebarprovider to home

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,7 +1,14 @@
+import { SidebarProvider } from '@/components/ui/sidebar';
 import { SidebarLayout } from '@/layout/sidebarlayout';
 
 const Homepage = () => {
-  return <SidebarLayout />;
+  return (
+    <div className="grid grid-cols-2">
+      <SidebarProvider>
+        <SidebarLayout />
+      </SidebarProvider>
+    </div>
+  );
 };
 
 export { Homepage };
