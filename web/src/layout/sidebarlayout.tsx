db05be6b2ba9e0704128993e5c3bed32b6162384
gabrielberlucci <gabrielberluccidev@proton.me>
2026-02-28 21:02:38 -0300

    feat: remove sidebarprovider, as it will be used in homepage

--- web/src/layout/sidebarlayout.tsx
+++ web/src/layout/sidebarlayout.tsx
@@ -5,7 +5,6 @@ import {
   SidebarContent,
   SidebarGroup,
   SidebarHeader,
-  SidebarProvider,
 } from '@/components/ui/sidebar';
 import {
   FileText,
@@ -18,77 +17,75 @@ import {
 
 const SidebarLayout = () => {
   return (
-    <SidebarProvider>
-      <Sidebar className="bg-[#09090B]">
-        <SidebarHeader>
-          <div className="flex gap-4 items-center">
-            <Avatar>
-              <AvatarImage src="https://i.pinimg.com/736x/42/fc/a3/42fca3c24df31ddf6b1dec5ce7675227.jpg" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+    <Sidebar className="bg-[#09090B]">
+      <SidebarHeader>
+        <div className="flex gap-4 items-center">
+          <Avatar>
+            <AvatarImage src="https://i.pinimg.com/736x/42/fc/a3/42fca3c24df31ddf6b1dec5ce7675227.jpg" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
 
-            <div>
-              <h1 className="text-white">Emissor NFe</h1>
-              <p className="text-gray-400">Plano Enterprise</p>
-            </div>
+          <div>
+            <h1 className="text-white">Emissor NFe</h1>
+            <p className="text-gray-400">Plano Enterprise</p>
           </div>
-        </SidebarHeader>
+        </div>
+      </SidebarHeader>
 
-        <SidebarContent className="text-white">
-          <SidebarGroup>
-            <Button
-              className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
-              size="sm"
-            >
-              <LayoutDashboard /> Dashboard
-            </Button>
-          </SidebarGroup>
+      <SidebarContent className="text-white">
+        <SidebarGroup>
+          <Button
+            className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
+            size="sm"
+          >
+            <LayoutDashboard /> Dashboard
+          </Button>
+        </SidebarGroup>
 
-          <SidebarGroup>
-            <Button
-              className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
-              size="sm"
-            >
-              <FileText /> Notas Fiscais
-            </Button>
-          </SidebarGroup>
-          <SidebarGroup>
-            <Button
-              className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
-              size="sm"
-            >
-              <UsersRound /> Clientes
-            </Button>
-          </SidebarGroup>
-          <SidebarGroup>
-            <Button
-              className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
-              size="sm"
-            >
-              <PackageSearch /> Produtos
-            </Button>
-          </SidebarGroup>
+        <SidebarGroup>
+          <Button
+            className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
+            size="sm"
+          >
+            <FileText /> Notas Fiscais
+          </Button>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Button
+            className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
+            size="sm"
+          >
+            <UsersRound /> Clientes
+          </Button>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Button
+            className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
+            size="sm"
+          >
+            <PackageSearch /> Produtos
+          </Button>
+        </SidebarGroup>
 
-          <SidebarGroup>
-            <Button
-              className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
-              size="sm"
-            >
-              <Settings /> Configuração
-            </Button>
-          </SidebarGroup>
+        <SidebarGroup>
+          <Button
+            className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
+            size="sm"
+          >
+            <Settings /> Configuração
+          </Button>
+        </SidebarGroup>
 
-          <SidebarGroup>
-            <Button
-              className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
-              size="sm"
-            >
-              <LogOut /> Sair
-            </Button>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar>
-    </SidebarProvider>
+        <SidebarGroup>
+          <Button
+            className="hover:bg-[#5B13EC10] hover:text-[#5B13EC] justify-start"
+            size="sm"
+          >
+            <LogOut /> Sair
+          </Button>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
   );
 };
 
